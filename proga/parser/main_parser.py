abbd0b5c3601672b837d214cfe10a11de53485a4
Egor <egorkononec64@gmail.com>
2024-08-07 13:51:15 +0300

    [m] edit parser to scrapingbee

--- proga/parser/main_parser.py
+++ proga/parser/main_parser.py
@@ -1,6 +1,8 @@
 import json
 
+import requests
 from botasaurus.browser import browser, Driver
+from bs4 import BeautifulSoup
 from requests import request
 
 from pyairtable import Table
@@ -33,19 +35,68 @@ def get_all_table_urls() -> list[str]:
     return [f"{item['fields']['URL']}" for item in records]
 
 
-@browser(
-    add_arguments=['--disable-extensions', '--disable-application-cache', '--disable-gpu', '--no-sandbox',  '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
-    close_on_crash=True,
-    profile='Carousell'
-)
-def carousell_parser(driver: Driver, data: str) -> None:
-    driver.get_via(data, 'https://www.carousell.ph')
-    #  <-- Cloudflare -->
-    driver.sleep(randint(25, 30))
+# @browser(
+#     add_arguments=['--disable-extensions', '--disable-application-cache', '--disable-gpu', '--no-sandbox',  '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+#     close_on_crash=True,
+#     profile='Carousell'
+# )
+# def carousell_parser(driver: Driver, data: str) -> None:
+#     driver.get_via(data, 'https://www.carousell.ph')
+#     #  <-- Cloudflare -->
+#     driver.sleep(randint(25, 30))
+#
+#     with open('test.js', 'r', encoding='utf-8') as f:
+#         code = f.read()
+#         result = driver.run_js(code)
+#
+#     data_ = get_all_table_urls()
+#     for dct in result:
+#         print(dct['url'] not in data_)
+#         if dct['url'] not in data_:
+#             print(dct)
+#             send_to_airtable(data={
+#                     "records": [
+#                         {
+#                             "fields": {
+#                                 "Name": dct['name'],
+#                                 "Price": dct['price'],
+#                                 "URL": dct['url'],
+#                                 "Photo": [
+#                                     {
+#                                         "url": dct['image'],
+#                                     }
+#                                 ]
+#                             }
+#                         }
+#                     ]
+#                 }
+#             )
+#     return
 
-    with open('test.js', 'r', encoding='utf-8') as f:
-        code = f.read()
-        result = driver.run_js(code)
+def carousell_parser(data: str):
+    response = requests.get(
+        url='https://app.scrapingbee.com/api/v1/',
+        params={
+            'api_key': 'KB7FUUC6C41UNZK19DIHNAICI6X9EGVOZ2VO0LCSF2RHLQL5IJLH92G66UK02OCABQASD16MUW0PKE3D',
+            'url': 'https://www.carousell.ph/search/rick%20owens',
+            'premium_proxy': 'true',
+            'country_code': 'sg'
+        },
+
+    )
+    result: list[dict] = []
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    for item in soup.select('div[data-testid*=listing-card]'):
+        __id = int(item.select('a')[-1].get('href').split('/')[2].split('-')[-1])
+        result.append({
+            "unique_id": __id,
+            "image": item.select('img')[-1].get('src'),
+            "name": item.select('p')[2].text,
+            "price": int(item.select('p')[3].text.replace('PHP ', '').replace(',', '')),
+            "url": f"https://www.carousell.ph/p/{__id}",
+        })
+        continue
 
     data_ = get_all_table_urls()
     for dct in result:
@@ -69,6 +120,8 @@ def carousell_parser(driver: Driver, data: str) -> None:
                     ]
                 }
             )
+        else:
+            break
     return
 
 
--- test.py
+++ test.py
@@ -1,113 +1,48 @@
-import json
-from ast import literal_eval
-
+#  Install the Python Requests library:
+# pip install requests
 import requests
-from requests import request
-
-dct = {
-    'Name': 'RICK OWENS CHAIN BRACELET IN STERLING SILVER  (authentic)',
-    'Price': 6.0,
-    'URL': 'https://www.carousell.ph/p/1309928650',
-    'Photo': 'https://media.karousell.com/media/photos/products/2024/6/21/rick_owens_chain_bracelet_in_s_1718943509_f87332e6.jpg'
-}
+from bs4 import BeautifulSoup
+import lxml
+from pprint import pprint
 
 
-def send_to_airtable(data: dict):
-    AIRTABLE_TOKEN = 'patGlHaDBAM9XQBxg.96fcbaa9fdb4c971731ccf5dd9cefa7ccc3842a0c74435f3b053b7e26d97eef3'
-    AIRTABLE_BASE_ID = 'appP155mkCHYI28rl'
-    AIRTABLE_URL = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}"
+def send_request():
+    # response = requests.get(
+    #     url='https://app.scrapingbee.com/api/v1/',
+    #     params={
+    #         'api_key': 'KB7FUUC6C41UNZK19DIHNAICI6X9EGVOZ2VO0LCSF2RHLQL5IJLH92G66UK02OCABQASD16MUW0PKE3D',
+    #         'url': 'https://www.carousell.ph/search/rick%20owens',
+    #         'premium_proxy': 'true',
+    #         'country_code': 'sg'
+    #     },
+    #
+    # )
+    # with open('index.html', 'w', encoding='utf-8') as f:
+    #     f.write(response.text)
 
-    # del data['id']
+    with open('index.html', 'r', encoding='utf-8') as f:
+        content = f.read()
 
-    url = f"{AIRTABLE_URL}/carousell_table"
-    headers = {
-      'Authorization': f'Bearer {AIRTABLE_TOKEN}',
-      'Content-Type': 'application/json'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=json.dumps(data))
-
-    return response
-
-
-new_data = {
-    "records": [
-        {
-            "fields": {
-                'Name': 'RICK OWENS CdsfgHAIN BRACELET IN STERLING SILVER  (authentic)',
-                'Price': 6,
-                'URL': 'https://www.carousell.ph/p/1309928650',
-                'Photo': [
-                    {
-                        'url': 'https://media.karousell.com/media/photos/products/2024/6/21/rick_owens_chain_bracelet_in_s_1718943509_f87332e6.jpg'
-                    }
-                ]
-            }
-        },
-        # {
-        #     "fields": {
-        #         'Name': 'RICK OWENS CdsfgHAIN BRACELET IN STERLING SILVER  (authentic)',
-        #         'Price': 63.0,
-        #         'URL': 'httdsfgps://www.carousell.ph/p/1309928650',
-        #         'Photo': 'https://meddsfgia.karousell.com/media/photos/products/2024/6/21/rick_owens_chain_bracelet_in_s_1718943509_f87332e6.jpg'
-        #     }
-        # }
-    ]
-}
-
-# for _ in range(3):
-#     print(send_to_airtable(data={
-#         "records": [
-#             {
-#                 "fields": {
-#                     'Name': 'RICK OWENS CdsfgHAIN BRACELET IN STERLING SILVER  (authentic)',
-#                     'Price': 6,
-#                     'URL': 'https://www.carousell.ph/p/1309928650',
-#                     'Photo': [
-#                         {
-#                             'url': 'https://media.karousell.com/media/photos/products/2024/6/21/rick_owens_chain_bracelet_in_s_1718943509_f87332e6.jpg'
-#                         }
-#                     ]
-#                 }
-#             },
-#         ]
-#     }))
+    soup = BeautifulSoup(content, 'lxml')
+    mass = []
+    cnt = 0
+    for item in soup.select('div[data-testid*=listing-card]'):
+        cnt += 1
+        print(cnt)
+        __id = int(item.select('a')[-1].get('href').split('/')[2].split('-')[-1])
+        print(__id)
+        mass.append({
+            "unique_id": __id,
+            "image": item.select('img')[-1].get('src'),
+            "name": item.select('p')[2].text,
+            "price": int(item.select('p')[3].text.replace('PHP ', '').replace(',', '')),
+            "url": f"https://www.carousell.ph/p/{__id}",
+        })
+        continue
+    pprint(mass)
+    print(len(mass))
+    # print('Response HTTP Status Code: ', response.status_code)
+    # print('Response HTTP Response Body: ', response.content)
 
 
-# with open('index.html', 'w', encoding='utf-8') as f:
-#     f.write(driver.page_html)
-# driver.sleep(10)
-from pyairtable import Table
-
-
-def foo():
-    AIRTABLE_TOKEN = 'patGlHaDBAM9XQBxg.96fcbaa9fdb4c971731ccf5dd9cefa7ccc3842a0c74435f3b053b7e26d97eef3'
-    AIRTABLE_BASE_ID = 'appP155mkCHYI28rl'
-    AIRTABLE_URL = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}"
-
-    url = f"{AIRTABLE_URL}/carousell_table?maxRecords=300&offset"
-    headers = {
-        'Authorization': f'Bearer {AIRTABLE_TOKEN}',
-        'Content-Type': 'application/json',
-    }
-
-    data = request("GET", url, headers=headers)
-    request('DELETE', url, headers=headers)
-    table = None
-    table = Table(api_key=AIRTABLE_TOKEN, base_id=AIRTABLE_BASE_ID, table_name='carousell_table')
-    records = table.all()
-
-    # print(*[f"{item['fields']['URL']}" for item in records], sep='\n')
-
-    return literal_eval(data.text)['records']
-
-
-if __name__ == '__main__':
-    from pprint import pprint
-    # pprint(foo())
-    # mass_id = [item['fields']['URL'] for item in foo()]
-    # print(len(mass_id))
-    # print(mass_id[1])
-    # for item in foo():
-    #     print(item['fields']['URL'])
-    #     break
+send_request()
